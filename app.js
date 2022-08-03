29ee258be7d87280378101d06ad5919fbf7654bd
TaiLee <leductai2201@gmail.com>
2022-08-03 16:29:25 +0700

    PORT fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const routes = require("./Routes/routes");
 const dotenv = require("dotenv");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // libs setup
 dotenv.config();
